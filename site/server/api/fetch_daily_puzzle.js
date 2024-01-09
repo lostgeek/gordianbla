6de7dfb9a8e83850746be2846ad6faa4c8fd84d1
Oguz Han Asnaz <github@lostgeek.de>
2024-01-09 14:15:16 +0100

    Fetching thumbnails

--- site/server/api/fetch_daily_puzzle.js
+++ site/server/api/fetch_daily_puzzle.js
@@ -8,7 +8,7 @@ export default defineEventHandler((event) => {
     var midnight = new Date();
     midnight.setHours(23, 59, 59, 999);
 
-    var maxAge = Math.floor((midnight-now)/1000);
+    const maxAge = Math.floor((midnight-now)/1000);
 
     // Check for query param 'n'
     const query = getQuery(event);
@@ -22,9 +22,9 @@ export default defineEventHandler((event) => {
         days = Math.floor((now-start) / 1000/60/60/24);
     }
 
-    var puzzle_id = days.toString().padStart(5, "0");
-    var filepath = `./assets/daily_puzzles/${puzzle_id}.svg.gz`;
-    var filebuffer = fs.readFileSync(filepath);
+    const puzzle_id = days.toString().padStart(5, "0");
+    const filepath = `./assets/daily_puzzles/${puzzle_id}.svg.gz`;
+    const filebuffer = fs.readFileSync(filepath);
     var uncompressed = zlib.unzipSync(filebuffer);
     uncompressed = Buffer.concat([Buffer.from(`<!--daily: ${puzzle_id}-->\n`), uncompressed]);
 
---
+++ site/server/api/fetch_daily_thumbnail.js
@@ -0,0 +1,28 @@
+import fs from "node:fs";
+
+export default defineEventHandler(async (event) => {
+    const query = getQuery(event);
+
+    var days;
+    if (query.n) {
+        days = parseInt(query.n);
+        if (isNaN(days)) {
+            return { error: "Query parameter n must be an integer." };
+        }
+    } else {
+        return { message: "Daily number 'n' not specified." };
+    }
+
+    const puzzle_id = days.toString().padStart(5, "0");
+
+    const dailyFolder = "./assets/daily_puzzles/";
+
+    const thumbFilepath = dailyFolder + `thumb/${puzzle_id}.png`;
+
+    try {
+        const data = fs.readFileSync(thumbFilepath);
+        return data;
+    } catch (err) {
+        return {message: "Could not read thumbnail.", error: err.toString()}
+    }
+});
