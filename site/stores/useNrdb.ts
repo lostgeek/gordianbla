e4b27e7dea8082c81c09eafcaf84ffb2077662cf
Oguz Han Asnaz <github@lostgeek.de>
2025-03-10 11:02:56 +0100

    fix: move to ts

--- site/stores/useNrdb.ts
+++ site/stores/useNrdb.ts
@@ -1,13 +1,45 @@
-const nrdbBaseUrl
-    = process.env.NODE_ENV === 'production'
-      ? 'https://netrunnerdb.com/api/2.0/public'
-      : '/dev_nrdb_api'
+// const nrdbBaseUrl
+//     = process.env.NODE_ENV === 'production'
+//       ? 'https://netrunnerdb.com/api/2.0/public'
+//       : '/dev_nrdb_api'
+const nrdbBaseUrl = 'https://netrunnerdb.com/api/2.0/public'
+
+export interface NrdbCard {
+  code: string
+  deck_limit: number
+  faction_code: string
+  faction_cost: number
+  pack_code: string
+  position: number
+  quantity: number
+  side_code: string
+  stripped_title: string
+  title: string
+  type_code: string
+  uniqueness: boolean
+  flavor?: string
+  influence_limit?: number
+  keywords?: string
+  minimum_deck_size?: number
+  illustrator?: string
+  stripped_text?: string
+  text?: string
+}
+
+export interface NrdbPack {
+  code: string
+  cycle_code: string
+  date_release: string
+  name: string
+  position: number
+  size: number
+}
 
 export const useNrdb = defineStore('nrdbStore', {
   state: () => ({
-    cards: [],
+    cards: [] as Array<NrdbCard>,
     imageUrlTemplate: '',
-    packs: [],
+    packs: [] as Array<NrdbPack>,
   }),
   getters: {
     packsInFormat(nrdb) {
@@ -41,24 +73,24 @@ export const useNrdb = defineStore('nrdbStore', {
     },
   },
   actions: {
-    packsInCycles(cycleCodes) {
+    packsInCycles(cycleCodes: string[]) {
       return this.packs.filter(p => cycleCodes.includes(p.cycle_code))
     },
     async fetch(reload = false) {
       if (this.cards.length === 0 || reload) {
         const data = await $fetch(
                     `${nrdbBaseUrl}/cards`,
-        )
+        ) as any
 
-        this.cards = data.data
+        this.cards = (data?.data ?? [])
         this.imageUrlTemplate = data.imageUrlTemplate
       }
       if (this.packs.length === 0 || reload) {
         const data = await $fetch(
                     `${nrdbBaseUrl}/packs`,
-        )
+        ) as any
 
-        this.packs = data.data
+        this.packs = (data?.data ?? [])
       }
     },
   },
